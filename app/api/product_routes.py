0ee517d42faed9e60d1f136c7af233f95a87a80a
Jashan2233 <jashan17100@gmail.com>
2023-10-03 15:15:52 -0500

    added get single product by its ID

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -6,6 +6,8 @@ from datetime import date
 
 product_routes= Blueprint('products', __name__)
 
+
+
 @product_routes.route('/')
 def get_products():
 #Get all Products!
@@ -13,10 +15,26 @@ def get_products():
     products = Product.query.all()
     res = [single_product.to_dict() for single_product in products]
 
-# Adds Owner Name to product!
+# Adds Owner Name + id to product!
     for product in res:
         owner_id = product['owner_id']
         user = User.query.get(owner_id)
         product_owner = user.to_dict()
         product['owner_info'] = product_owner['username']
     return res
+
+
+@product_routes.route('/<int:product_id>')
+def get_single_product(product_id):
+
+    single_product = Product.query.get(product_id)
+    product = single_product.to_dict()
+
+    #Adds owner name to the Product
+
+    owner_id = product['owner_id']
+    user = User.query.get(owner_id)
+    product_owner = user.to_dict()
+    product['owner_info'] = product_owner['username']
+
+    return product
