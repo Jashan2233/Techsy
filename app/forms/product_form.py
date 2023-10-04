71bd427807b0f1ddffc614b61d48b3edc41bc4da
Jashan2233 <jashan17100@gmail.com>
2023-10-04 10:55:09 -0500

    added aws to form validations

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import IntegerField, StringField, SubmitField, DecimalField
 from wtforms.validators import DataRequired, NumberRange, Length
-# from ..api.aws_helpers import ALLOWED_EXTENSIONS
+from ..api.aws_helpers import ALLOWED_EXTENSIONS
 
 class ProductForm(FlaskForm):
     name= StringField('Name', validators=[DataRequired(), Length(min=4, max=50)])
     description = StringField('Description', validators=[DataRequired(), Length(min=10, max=100)])
     price = IntegerField('Price', validators=[DataRequired()])
-    preview_image = StringField('Image File', validators=[DataRequired()])
+    preview_image = FileField('Image File', validators=[DataRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     submit = SubmitField('Submit')
