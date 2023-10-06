ba0424c132611c28d82b5af322283a9e8810121d
Jashan2233 <jashan17100@gmail.com>
2023-10-06 10:03:07 -0500

    added store and reducers for reviews

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -29,3 +29,99 @@ export const actionUpdateUserReview = (updated_review) => ({
   type: UPDATE_USER_REVIEW,
   updated_review,
 });
+
+// THUNKS
+export const thunkGetUserReviews = (user_id) => async (dispatch) => {
+  const response = await fetch(`/api/reviews/user/${user_id}`);
+
+  if (response.ok) {
+    const user_reviews = await response.json();
+    dispatch(actionGetUserReviews(user_reviews));
+  }
+};
+
+export const thunkGetProductReviews = (product_id) => async (dispatch) => {
+  const response = await fetch(`/api/reviews/product/${product_id}`);
+
+  if (response.ok) {
+    const product_reviews = await response.json();
+    dispatch(actionGetProductReviews(product_reviews));
+  }
+};
+
+export const thunkCreateProductReview =
+  (product_id, review) => async (dispatch) => {
+    const response = await fetch(`/api/reviews/new/product/${product_id}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(review),
+    });
+
+    if (response.ok) {
+      const new_review = await response.json();
+      dispatch(actionCreateProductReview(new_review));
+    }
+  };
+
+export const thunkDeleteReview = (reviewId) => async (dispatch) => {
+  const response = await fetch(`/api/reviews/${reviewId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    dispatch(actionDeleteReview(reviewId));
+    return reviewId;
+  }
+};
+
+export const thunkUpdateUserReview =
+  (review_id, newReview) => async (dispatch) => {
+    const response = await fetch(`/api/reviews/${review_id}/update`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newReview),
+    });
+
+    if (response.ok) {
+      const updated_review = await response.json();
+      dispatch(actionUpdateUserReview(updated_review));
+    }
+  };
+
+const initialState = { productReviews: {}, newReview: {}, userReviews: {} };
+
+const productReviewsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PRODUCT_REVIEWS:
+      return { ...state, productReviews: action.reviews };
+
+    case CREATE_PRODUCT_REVIEW:
+      return { ...state, newReview: action.new_review };
+
+    case GET_USER_REVIEWS:
+      return { ...state, userReviews: action.user_reviews };
+
+    case DELETE_REVIEW:
+      const { [action.reviewId]: deletedReview, ...restReviews } =
+        state.userReviews;
+      return { ...state, userReviews: restReviews, newReview: {} };
+
+    case UPDATE_USER_REVIEW:
+      return {
+        ...state,
+        productReviews: {
+          ...state.productReviews,
+          [action.updated_review.id]: action.updated_review,
+        },
+        userReviews: {
+          ...state.userReviews,
+          [action.updated_review.id]: action.updated_review,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default productReviewsReducer;
